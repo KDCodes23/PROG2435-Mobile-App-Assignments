f9878eb8cc93f94196158c3681988d9319e28e6d
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-09-30 16:25:59 -0400

    Complete classes

--- assignment1/lib/trip_planner.dart
+++ assignment1/lib/trip_planner.dart
@@ -1,4 +1,20 @@
-import 'dart:collection';
+Map<String, Map> packages = {
+  "Individual": {
+    "Blue Mountain": 200,
+    "Niagara": 100,
+    "Banff National Park": 300,
+  },
+  "Family": {
+    "Blue Mountain": 400,
+    "Niagara": 300,
+    "Banff National Park": 700,
+  },
+  "Group": {
+    "Blue Mountain": 700,
+    "Niagara": 1000,
+    "Banff National Park": 1300,
+  }
+};
 
 class Customer {
   String destination;
@@ -8,6 +24,14 @@ class Customer {
 
   Customer(
       this.destination, this.contactName, this.contactPhone, this.tripPrice);
+
+  void bookTravel() {
+    print("Travel booked for $contactName to $destination.");
+  }
+
+  void arrangeTransport() {
+    print("Transport has been arranged for $contactName");
+  }
 }
 
 class Individual extends Customer {
@@ -16,16 +40,27 @@ class Individual extends Customer {
   Individual(super.destination, super.contactName, super.contactPhone,
       super.tripPrice, this.policyNumber);
 
-  void NotifyWorkplace() {
-    print("Your workplace has been notified");
+  @override
+  void bookTravel() {
+    super.bookTravel();
+    print("$contactName's workplace has been notified");
+    print("Your insurance policy number is: $policyNumber");
   }
 }
 
 class Family extends Customer {
   String insuranceCompany;
+  String memberInCanada;
 
   Family(super.destination, super.contactName, super.contactPhone,
-      super.tripPrice, this.insuranceCompany);
+      super.tripPrice, this.insuranceCompany, this.memberInCanada);
+
+  @override
+  void bookTravel() {
+    super.bookTravel();
+    print("Health coverage is provided by $insuranceCompany");
+    print("Family member remaining in Canada: $memberInCanada");
+  }
 }
 
 class Group extends Customer {
@@ -33,19 +68,11 @@ class Group extends Customer {
 
   Group(super.destination, super.contactName, super.contactPhone,
       super.tripPrice, this.materialRequired);
-}
 
-Map<String, Map> packages = {
-  "Individual": {"Blue Mountain": 200,
-                 "Niagara": 100,
-                 "Banff National Park": 300,
-                 },
-  "Family": {"Blue Mountain": 400,
-                 "Niagara": 300,
-                 "Banff National Park": 700,
-                 },
-  "Group": {"Blue Mountain": 700,
-                 "Niagara": 1000,
-                 "Banff National Park": 1300,
-                 }
-};
+  @override
+  void bookTravel() {
+    super.bookTravel();
+    print("Destination company notified! Group leader is: $contactName");
+    print("Organized hardware: $materialRequired");
+  }
+}
