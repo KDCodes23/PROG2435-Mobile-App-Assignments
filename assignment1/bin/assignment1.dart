131470230bd8cfa8fb6e8f960bc21cc3d1fbab86
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-09-30 15:54:01 -0400

    Add stuff in main file

--- assignment1/bin/assignment1.dart
+++ assignment1/bin/assignment1.dart
@@ -1,5 +1,36 @@
+import 'dart:io';
+
 import 'package:assignment1/trip_planner.dart' as trip_planner;
 
 void main(List<String> arguments) {
   // Main code starts here...
+  int iter = 0;
+  print("Select packages for:");
+  for (String category in trip_planner.packages.keys) {
+    print("${++iter} $category");
+  }
+  iter = 0;
+  String? categoryType = stdin.readLineSync();
+  String category = "";
+  switch (categoryType!) {
+    case "1":
+      category = "Individual";
+      break;
+    case "2":
+      category = "Family";
+      break;
+    case "3":
+      category = "Group";
+      break;
+    default:
+      break;
+  }
+  print("Select packages:");
+  for (String package in trip_planner.packages[category]!.keys) {
+    print(
+        "${++iter} $package - \$${trip_planner.packages[category]![package]}");
+  }
+  iter = 0;
+  String? selectedPackage = stdin.readLineSync();
+  
 }
--- assignment1/lib/trip_planner.dart
+++ assignment1/lib/trip_planner.dart
@@ -1,10 +1,13 @@
+import 'dart:collection';
+
 class Customer {
   String destination;
   String contactName;
   String contactPhone;
   double tripPrice;
 
-  Customer(this.destination, this.contactName, this.contactPhone, this.tripPrice);  
+  Customer(
+      this.destination, this.contactName, this.contactPhone, this.tripPrice);
 }
 
 class Individual extends Customer {
@@ -12,6 +15,10 @@ class Individual extends Customer {
 
   Individual(super.destination, super.contactName, super.contactPhone,
       super.tripPrice, this.policyNumber);
+
+  void NotifyWorkplace() {
+    print("Your workplace has been notified");
+  }
 }
 
 class Family extends Customer {
@@ -27,3 +34,18 @@ class Group extends Customer {
   Group(super.destination, super.contactName, super.contactPhone,
       super.tripPrice, this.materialRequired);
 }
+
+Map<String, Map> packages = {
+  "Individual": {"Blue Mountain": 200,
+                 "Niagara": 100,
+                 "Banff National Park": 300,
+                 },
+  "Family": {"Blue Mountain": 400,
+                 "Niagara": 300,
+                 "Banff National Park": 700,
+                 },
+  "Group": {"Blue Mountain": 700,
+                 "Niagara": 1000,
+                 "Banff National Park": 1300,
+                 }
+};
